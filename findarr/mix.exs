64a5ccbd68cda375d59231caf00653c12b952a89
Dragos Musetescu <dragos@musetescu.name>
2021-07-21 19:00:20 +0300

    add Service

--- findarr/mix.exs
+++ findarr/mix.exs
@@ -5,7 +5,7 @@ defmodule Findarr.MixProject do
     [
       app: :findarr,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
