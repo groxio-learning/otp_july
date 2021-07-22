2c1e0c820760248fc44b1bca4032586e718e03bb
James Kienle <james.kienle@gmail.com>
2021-07-22 08:03:31 -0700

    Set up Mix Project and Create Board API

---
+++ mindmastarr/lib/mindmastarr.ex
@@ -0,0 +1,18 @@
+defmodule Mindmastarr do
+  @moduledoc """
+  Documentation for `Mindmastarr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mindmastarr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
